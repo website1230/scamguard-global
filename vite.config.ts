19c52d273c2c82538882c676dad6e71c3b9c60f3
shivam rajput <rajputshivam845101@gmail.com>
2026-01-04 22:11:01 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  // 🔴 MOST IMPORTANT FOR GITHUB PAGES
+  base: '/scamguard-global/',
+
+  plugins: [react()],
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+})
